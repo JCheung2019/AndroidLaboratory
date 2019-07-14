f1dd0d41cd474e601c6fa4d0e6833fc144bc390c
J.Cheung <571783323@qq.com>
2019-07-14 11:56:15 +0800

    修改

--- app/src/main/java/com/zjun/laboratory/template/MainActivity.java
+++ app/src/main/java/com/zjun/laboratory/template/MainActivity.java
@@ -3,6 +3,8 @@ package com.zjun.laboratory.template;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,5 +21,9 @@ public class MainActivity extends AppCompatActivity {
      */
     private void initialize() {
 
+    }
+
+    public void test(View view) {
+        Toast.makeText(this, "我是Button", Toast.LENGTH_SHORT).show();
     }
 }
